7fab5ad5d636c075a61be37dc6cd7ad371c696ef
Henrique Dias <mrhdias@gmail.com>
2019-03-23 11:37:10 +0000

    Small cosmetic change.

--- examples/koch_curve.nim
+++ examples/koch_curve.nim
@@ -12,7 +12,6 @@ const
   iterations = 4
 
 proc kochCurve(img: gdImagePtr, x1: float, y1: float, x2: float, y2: float, iter: int): void  =
-  var i = iter
   let angle = PI / 3 # 60 degrees
   let x3 = (x1 * 2 + x2) / 3
   let y3 = (y1 * 2 + y2) / 3
@@ -21,12 +20,11 @@ proc kochCurve(img: gdImagePtr, x1: float, y1: float, x2: float, y2: float, iter
   let x5 = x3 + (x4 - x3) * cos(angle) + (y4 - y3) * sin(angle)
   let y5 = y3 - (x4 - x3) * sin(angle) + (y4 - y3) * cos(angle)
 
-  if i > 0:
-    i -= 1
-    img.kochCurve(x1, y1, x3, y3, i)
-    img.kochCurve(x3, y3, x5, y5, i)
-    img.kochCurve(x5, y5, x4, y4, i)
-    img.kochCurve(x4, y4, x2, y2, i)
+  if iter > 0:
+    img.kochCurve(x1, y1, x3, y3, iter - 1)
+    img.kochCurve(x3, y3, x5, y5, iter - 1)
+    img.kochCurve(x5, y5, x4, y4, iter - 1)
+    img.kochCurve(x4, y4, x2, y2, iter - 1)
   else:
     img.gdLine(startPoint=[x1.int, y1.int], endPoint=[x3.int, y3.int])
     img.gdLine(startPoint=[x3.int, y3.int], endPoint=[x5.int, y5.int])
