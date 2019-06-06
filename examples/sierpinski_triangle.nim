748f3e74279a4a9d489aac49ece9f275b1efdcf3
Henrique Dias <mrhdias@gmail.com>
2019-06-06 19:25:15 +0100

    Added the keyword "discard" to the img.backgroundColor and img.foregroundColor.
    Added the keyword "discard" to the img.backgroundColor and img.foregroundColor to remove the message "'white/blue' is declared but not used".

--- examples/sierpinski_triangle.nim
+++ examples/sierpinski_triangle.nim
@@ -13,8 +13,8 @@ const
 proc main() =
 
   withGd imageCreate(width + margin * 2, height + margin * 2) as img:
-    let white = img.backgroundColor(0xffffff)
-    let blue = img.foregroundColor(0x0000ff)
+    discard img.backgroundColor(0xffffff)
+    discard img.foregroundColor(0x0000ff)
 
     for y in 0 .. width-1:
       for x in 0 .. width-1:
