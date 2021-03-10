e2f422dd128419991aec94ea75257f9cdf3aadc0
Henrique Dias <mrhdias@gmail.com>
2021-03-10 18:13:16 +0000

    Small fixe!

--- tests/image_flip.nim
+++ tests/image_flip.nim
@@ -10,7 +10,6 @@ proc main() =
   discard existsOrCreateDir(outputsDir)
 
   withGd imageCreate(300, 300) as img:
-    let white = img.setColor(255, 255, 255)
 
     let red = img.setColor(255, 0, 0)
     img.drawRectangle(startCorner=[50, 50], endCorner=[100, 220], color=red)
